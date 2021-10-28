7fb8dbb637ea54ff166b2fe231c434a89c6db3d4
john-smilga <johnsmilga@gmail.com>
2021-10-28 12:27:46 -0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,24 @@
+# Jobify App
+
+#### Initial Setup
+
+- create fron-end and server
+- setup react app in front-end 'npx create-react-app .'
+- remove the junk
+- get assets from final
+- install normalize.css
+- setup global styles
+
+#### Setup Pages
+
+- create pages directory
+- create list of pages
+- setup index.js
+
+#### React Router 6
+
+```bash
+npm i react-router@6.0.0-beta.0
+```
+
+- setup basic routing in App.js
---
+++ jobify/final/front-end/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div style={{ margin: '2rem' }}>
+      <h1>hello world</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        repellat dolorum commodi tempore repellendus similique deleniti
+        reiciendis, dicta rem obcaecati, mollitia laboriosam explicabo, incidunt
+        quaerat perferendis minima vitae accusantium adipisci.
+      </p>
+      <button className='btn'>hello world</button>
+    </div>
+  );
+}
+
+export default App;
